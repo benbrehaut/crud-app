dce527b0b98b76ec5668f77e011674937c6393e0
Ben Brehaut <bencbrehaut@gmail.com>
2019-07-20 16:37:39 +0100

    Fixing bug where new reg user would be kicked after refresh

--- src/actions/register.js
+++ src/actions/register.js
@@ -1,4 +1,7 @@
 import api from "../api";
 import { userLoggedIn } from './auth'
 
-export const register = (data) => (dispatch) => api.user.register(data).then(user => dispatch(userLoggedIn(user)));
+export const register = (data) => (dispatch) => api.user.register(data).then(user => { 
+  localStorage.bookwormJWT = user.token;
+  dispatch(userLoggedIn(user));
+});
